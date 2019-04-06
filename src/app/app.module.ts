ed321e0a2146225765b8c771543401421e396f1b
Vinod Thiagarajan <thiagarajan.v@husky.neu.edu>
2019-04-05 21:51:58 -0400

    login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { BlogComponent } from './blog/blog.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +23,8 @@ import { SignupComponent } from './signup/signup.component';
     AboutUsComponent,
     ContactUsComponent,
     BlogComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
