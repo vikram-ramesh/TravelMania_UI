8491546bd883e6d5c92ed2f72a369df5146f647b
RajashreeJoshi <joshi.raj@husky.neu.edu>
2019-04-08 21:10:12 -0400

    conflicts resolved

--- src/app/landing-page-component/landing-page-component.component.ts
+++ src/app/landing-page-component/landing-page-component.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FlightsService } from '../services/flights.service';
 import { Router } from '@angular/router';
-=======
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-landing-page-component',
@@ -14,34 +11,30 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class LandingPageComponentComponent implements OnInit {
-<<<<<<< Updated upstream
   flightSearchForm: FormGroup;
   flights: any = [];
-  constructor(private router: Router, private formBuilder: FormBuilder, private _flightsService: FlightsService) {
+  constructor(private router: Router, private formBuilder: FormBuilder, private _flightsService: FlightsService, private config: NgbCarouselConfig) {
     this.flightSearchForm = this.formBuilder.group({
       source: [''],
       destination: [''],
       date: ['']
     });
-=======
 
-  constructor(private config: NgbCarouselConfig) {
     this.config.showNavigationArrows = false;
     this.config.showNavigationIndicators = false;
->>>>>>> Stashed changes
   }
 
   ngOnInit() {
   }
 
 
-  onSubmit(){
-    console.log(this.flightSearchForm.value.source)
-    this._flightsService.getFlightBySourceAndDestination('Boston','LA')//this.flightSearchForm.value.source, this.flightSearchForm.value.destination
+  onSubmit() {
+    console.log(this.flightSearchForm.value.source);
+    this._flightsService.getFlightBySourceAndDestination('Boston', 'LA')// this.flightSearchForm.value.source, this.flightSearchForm.value.destination
      .subscribe(res => {
-       if(res){
+       if (res) {
         this.flights = res;
-        localStorage.setItem("flight",this.flights);
+        localStorage.setItem('flight', this.flights);
         this.router.navigate(['/searchResults']);
         console.log(this.flights[0].source);
        }
