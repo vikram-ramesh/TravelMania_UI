e026183b696486ef9bc34cf3c99a8de9144941a4
RajashreeJoshi <joshi.raj@husky.neu.edu>
2019-04-08 21:06:53 -0400

    landing page

--- src/app/landing-page-component/landing-page-component.component.ts
+++ src/app/landing-page-component/landing-page-component.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FlightsService } from '../services/flights.service';
 import { Router } from '@angular/router';
+=======
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-landing-page-component',
   templateUrl: './landing-page-component.component.html',
-  styleUrls: ['./landing-page-component.component.css']
+  styleUrls: ['./landing-page-component.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class LandingPageComponentComponent implements OnInit {
+<<<<<<< Updated upstream
   flightSearchForm: FormGroup;
   flights: any = [];
   constructor(private router: Router, private formBuilder: FormBuilder, private _flightsService: FlightsService) {
@@ -17,6 +23,12 @@ export class LandingPageComponentComponent implements OnInit {
       destination: [''],
       date: ['']
     });
+=======
+
+  constructor(private config: NgbCarouselConfig) {
+    this.config.showNavigationArrows = false;
+    this.config.showNavigationIndicators = false;
+>>>>>>> Stashed changes
   }
 
   ngOnInit() {
