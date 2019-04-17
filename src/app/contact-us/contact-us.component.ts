9b6b70d7072fb8a68f712bee02194951a915235b
chinmayjomraj <jomraj.c@husky.neu.edu>
2019-04-17 12:56:31 -0400

    added for EMAIL API

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
@@ -12,4 +13,19 @@ export class ContactUsComponent implements OnInit {
   ngOnInit() {
   }
 
+  shareEmail(fname: HTMLInputElement, message: HTMLInputElement):boolean{
+
+      emailjs.init('user_b0usKkuWEtIVQlb6wrVAd')
+
+    var service_id = 'default_service';
+     var template_id = 'template_YkodUUED';
+var template_params = {
+	to_name: fname.value,
+	reply_to: 'chinmay.jomraj7786@gmail.com',
+	message_html: message.value
+};
+
+     emailjs.send(service_id,template_id,template_params);
+    return false;
+  }
 }
